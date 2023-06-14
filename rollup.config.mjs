6a9b737376d0f8e3e80c40cb1d2024ca4145a336
ZeroDegress <zerodegress@outlook.com>
2023-06-14 09:26:20 +0800

    use @rollup/plugin-typescript

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,8 @@
-import json from '@rollup/plugin-json';
-import ts from 'rollup-plugin-ts';
-import terser from '@rollup/plugin-terser';
+import json from '@rollup/plugin-json'
+import ts from 'rollup-plugin-ts'
+import terser from '@rollup/plugin-terser'
+import typescript from '@rollup/plugin-typescript'
+import babel from '@rollup/plugin-babel'
 
 export default {
 	input: 'src/index.ts',
@@ -9,5 +11,5 @@ export default {
 		format: 'iife',
 		name: 'lib'
 	},
-	plugins: [json(), ts()]
+	plugins: [json(), typescript()]
 };
