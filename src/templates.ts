b0939d6875f63a5aca6ce55dd4120a08aab76de1
ZeroDegress <zerodegress@outlook.com>
2023-07-03 10:07:13 +0800

    增加一个新的模板

--- src/templates.ts
+++ src/templates.ts
@@ -1,10 +1,37 @@
 import { Log } from 'java:net.rwhps.server.util.log'
 import { createPlugin } from './plugin'
+import { createEvent } from './event'
 
+/**
+ * 插件模板：你好，世界！
+ * 
+ * 这个模板会在插件被启用时输出一行"Hello World!"
+ * @author ZeroDegres
+ * @returns 你好，世界！模板插件
+ */
 export function createHelloWorldPlugin() {
   return createPlugin({
     onEnable() {
       Log.clog('Hello World!')
     }
+  })
+}
+
+/**
+ * 插件模板: 铁锈，启动！
+ * 
+ * 这个模板会在游戏开始时输出一行"RustedWarefare, Start!"
+ * @author ZeroDegres
+ * @returns 铁锈，启动！模板插件
+ */
+export function createGameStartHelloPlugin() {
+  return createPlugin({
+    registerEvents() {
+      return createEvent({
+        registerGameStartEvent() {
+          Log.clog('RustedWarfare, Start!')
+        }
+      })
+    },
   })
 }
